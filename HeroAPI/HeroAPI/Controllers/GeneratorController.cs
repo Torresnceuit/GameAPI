1ebef0b77820b1ef5d2e4e605564d24228f71434
Torres <torresnceuit@gmail.com>
2018-03-23 10:41:06 +0700

    remove empty lines

--- HeroAPI/HeroAPI/Controllers/GeneratorController.cs
+++ HeroAPI/HeroAPI/Controllers/GeneratorController.cs
@@ -115,33 +115,24 @@ namespace PlayersAPI.Controllers
         [Route("drawfixture/{id}")]
         public IHttpActionResult DrawFixture(string id)
         {
-            //DELETE all data for the tournament first
-            List<Round> lRounds = db.Rounds.Where(h => h.TourId == id).ToList();
+			List<Round> lRounds = db.Rounds.Where(h => h.TourId == id).ToList();
             List<Match> lMatches = db.Matches.Where(h => h.TourId == id).ToList();
-            List<Rank> lRanks = db.Ranks.Where(h => h.TourId == id).ToList();
-             
-
-
+            List<Rank> lRanks = db.Ranks.Where(h => h.TourId == id).ToList(); 
+            // delete all data for the tournament first			
             foreach (Rank rank in lRanks)
             {
                 db.Ranks.Remove(rank);
             }
-
             foreach (Round round in lRounds)
             {
                 db.Rounds.Remove(round);
             }
-
             foreach (Match match in lMatches)
             {
                 db.Matches.Remove(match);
             }
 
             db.SaveChanges();
-
-
-
-
             //Start generate fixture after empty it
             int num_teams = db.Teams
                             .Where(h => h.TourId == id)
@@ -152,61 +143,69 @@ namespace PlayersAPI.Controllers
 
             int[,] results = GenerateRoundRobin(num_teams);
             int n = 0;
+			// n = 2, each team plays againts others 2 times: Home and Away
             while (n < 2)
             {
+				
                 for (int rounds = 0; rounds < num_teams - 1; rounds++)
                 {
+					// Create a round
                     Round round = new Round();
                     round.Name = (num_teams - 1) * n + rounds + 1;
                     round.TourId = id;
+					// add to Rounds db
                     db.Rounds.Add(round);
                     db.SaveChanges();
-
-
+					// each round, we generate pairs of teams
                     for (int teams = 0; teams < num_teams; teams++)
                     {
+						// Create a new match
                         Match match = new Match();
                         match.TourId = id;
+						// fist leg round
                         if (n == 0)
                         {
+							// assign Home team
                             match.HomeId = lTeams.ElementAt(teams).Id;
                             if (results[teams, rounds] >= 0)
+								// assign Away team
                                 match.AwayId = lTeams.ElementAt(results[teams, rounds]).Id;
                         }
+						 // 2nd leg round
                         else
                         {
+							// assgin Away team
                             match.AwayId = lTeams.ElementAt(teams).Id;
                             if (results[teams, rounds] >= 0)
+								// assign Home team
                                 match.HomeId = lTeams.ElementAt(results[teams, rounds]).Id;
                         }
-
                         match.RoundId = round.Id;
+						// check if match does not exist
                         if (db.Matches
                             .Where(h => (h.HomeId == match.HomeId || h.HomeId == match.AwayId) && h.RoundId == match.RoundId)
                             .ToList().Count == 0)
                         {
+							// add match to Matches db
                             db.Matches.Add(match);
                             db.SaveChanges();
 
                         }
-
-                    }
-                    
-
+                    }                   
                 }
                 n++;
             }
-
-
+			// return 200
             return Ok();
         }
         
         // Generate Round Robin Algorithm
         public int[,] GenerateRoundRobin(int num_teams)
         {
-
+			// Even number of teams
             if (num_teams % 2 == 0)
                 return GenerateRoundRobinEven(num_teams);
+			// Odd number of teams
             else
                 return GenerateRoundRobinOdd(num_teams);
         }
@@ -225,32 +224,18 @@ namespace PlayersAPI.Controllers
             // Start the rounds.
             for (int round = 0; round < num_teams; round++)
             {
-
-
                 for (int i = 0; i < n2; i++)
                 {
-
                     int team1 = teams[n2 - i];
-
                     int team2 = teams[n2 + i + 1];
-
                     results[team1, round] = team2;
                     results[team2, round] = team1;
-
-
-
                 }
-
                 // Set the team with the bye.
                 results[teams[0], round] = BYE;
-
-
                 // Rotate the array.
                 RotateArray(teams);
-
-                //Console.WriteLine(_teams.ToString());
             }
-
             return results;
         }
 
@@ -258,7 +243,6 @@ namespace PlayersAPI.Controllers
         private void RotateArray(int[] teams)
         {
             int tmp = teams[teams.Length - 1];
-
             Array.Copy(teams, 0, teams, 1, teams.Length - 1);
             teams[0] = tmp;
         }
@@ -267,15 +251,12 @@ namespace PlayersAPI.Controllers
             // circular.. Do Nothing
             if (places % list.Count == 0)
                 return;
-
             T[] copy = new T[list.Count];
             list.CopyTo(copy, 0);
-
             for (int i = 0; i < list.Count; i++)
             {
                 // % used to handle circular indexes and places > count case
                 int index = (i + places) % list.Count;
-
                 list[i] = copy[index];
             }
         }
@@ -295,10 +276,7 @@ namespace PlayersAPI.Controllers
                     if (results[team, round] == BYE)
                     {
                         // Change the bye to the new team.
-
                         results2[team, round] = num_teams - 1;
-
-
                         results2[num_teams - 1, round] = team;
                     }
                     else
